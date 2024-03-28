25fc175082b0a1b4bb82d5b24c539ace09b53ea5
Bao D. Van <32475885+duybaovan@users.noreply.github.com>
2024-03-28 16:32:37 -0700

    Update gig-job-board.tsx

--- src/app/_components/gig-job-board.tsx
+++ src/app/_components/gig-job-board.tsx
@@ -2,13 +2,13 @@ import { GigList } from "./gig-board-ui";
 import { trpc } from "~/utils/trpc";
 
 export function GigJobBoard() {
-  const publicGigsQuery = trpc.gig.getPublicGigs.useQuery();
+  const publicGigsQuery = trpc.gig.getGigs.useQuery({ take: 10 });
   const latestGigs = publicGigsQuery.data ?? [];
   return <GigList gigs={latestGigs} />;
 }
 
 export function PartialGigJobBoard() {
-  const publicGigsQuery = trpc.gig.getPublicGigs.useQuery();
+  const publicGigsQuery = trpc.gig.getGigs.useQuery({ take: 4 });
   const latestGigs = publicGigsQuery.data ?? [];
   return <GigList gigs={latestGigs} isPartial={true} />;
 }
