4ee779bc8d51b16b0020d4a3eeae7e6c4152b8fa
Bao D. Van <32475885+duybaovan@users.noreply.github.com>
2024-03-28 16:46:55 -0700

    verify physician credentials screen

--- src/app/doc_verify/page.tsx
+++ src/app/doc_verify/page.tsx
@@ -1,57 +1,50 @@
 import React from "react";
 import { Button } from "~/components/ui/button";
-import { Card } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 
 const VerifyPhysicianCredentials: React.FC = () => {
   return (
-    <div className="flex min-h-screen flex-col justify-center bg-gray-100">
-      <Card className="mx-auto max-w-lg p-8">
-        <header className="mb-6 flex items-center justify-between">
-          <Label className="Label-gray-800">sermo</Label>
-          <Button variant="ghost">Log in</Button>
-        </header>
-        <main>
-          <div className="mb-4 flex items-center space-x-2">
-            <Label>CREATE</Label>
-            <Label>&rarr;</Label>
-            <Label className="Label-purple-500">VERIFY</Label>
-            <Label>&rarr;</Label>
-            <Label>COMPLETE</Label>
-          </div>
-          <Label className="mb-2">Verify your physician credentials</Label>
-          <Label className="mb-4">
+    <main>
+      <Card className="mx-auto mt-12 max-w-lg p-12">
+        <CardHeader>
+          <CardTitle>Verify your physician credentials.</CardTitle>
+
+          <CardDescription className="mb-4">
             Please provide your NPI number to help us quickly verify your
             credentials.
-          </Label>
-          <div className="mb-4">
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="mb-6">
             <label
               htmlFor="npiNumber"
-              className="Label-sm Label-gray-700 block font-medium"
+              className="Label-sm Label-gray-700 mb-2 block font-medium"
             >
               NPI number
             </label>
             <Input id="npiNumber" placeholder="10 digit number" />
           </div>
-          <div className="mb-4 flex space-x-2">
-            <Button variant="outline">Skip this step</Button>
-            <Button variant="outline">Get verified Manually</Button>
-            <Button variant="outline">Dont have your NPI number?</Button>
-            <Button variant="outline">Enter your AMA number</Button>
-          </div>
-          <div className="flex justify-between">
-            <Button variant="ghost">Back</Button>
+
+          <div className="mb-6 flex justify-between">
             <Button>Next</Button>
           </div>
-        </main>
-        <footer className="Label-center Label-sm Label-gray-600 mt-6">
-          Need help? Contact us at support@sermo.com
-          <br />
-          Terms of Service - Privacy - Cookies
-        </footer>
+        </CardContent>
+        <CardFooter>
+          <Label className="text-slate-400">
+            Need help? Contact us at founders@harvi.io
+          </Label>
+        </CardFooter>
       </Card>
-    </div>
+    </main>
   );
 };
 
