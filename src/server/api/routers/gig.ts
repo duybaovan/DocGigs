f43a59fcf33917b130dd6dcea9e403a20352ef3b
Bao D. Van <32475885+duybaovan@users.noreply.github.com>
2024-03-28 16:34:23 -0700

    gig api change

--- src/server/api/routers/gig.ts
+++ src/server/api/routers/gig.ts
@@ -6,33 +6,18 @@ import {
 } from "~/server/api/trpc";
 
 export const gigRouter = createTRPCRouter({
-  getPublicGigs: publicProcedure.query(({ ctx }) => {
-    // Only return 5 gigs
-    return ctx.db.gig.findMany({
-      include: {
-        location: true, // Include location data
-      },
-      orderBy: { createdAt: "desc" },
-      take: 5,
-    });
-  }),
-
-  getMyGigs: protectedProcedure.query(({ ctx }) => {
-    // Fetch gigs related to the logged-in user
-    return ctx.db.gig.findMany({
-      where: {
-        users: {
-          some: {
-            id: ctx.session.user.id,
-          },
+  getGigs: publicProcedure
+    .input(z.object({ take: z.number().positive() }))
+    .query(({ ctx, input }) => {
+      // Only return a specified number of gigs
+      return ctx.db.gig.findMany({
+        include: {
+          location: true, // Include location data
         },
-      },
-      include: {
-        location: true, // Include location data
-      },
-      orderBy: { createdAt: "desc" },
-    });
-  }),
+        orderBy: { createdAt: "desc" },
+        take: input.take,
+      });
+    }),
 
   saveGig: protectedProcedure
     .input(z.object({ gigId: z.number() }))
