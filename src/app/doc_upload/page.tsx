99195997fd240cbef156e093f3ca8da929b070e3
Bao D. Van <32475885+duybaovan@users.noreply.github.com>
2024-04-02 12:39:10 -0700

    Finished with doc upload demo

--- src/app/doc_upload/page.tsx
+++ src/app/doc_upload/page.tsx
@@ -1,43 +1,74 @@
 "use client";
 import React, { useState } from "react";
-import { Card, CardHeader, CardTitle, CardContent } from "~/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "~/components/ui/card";
 
 import Uppy from "@uppy/core";
 import Webcam from "@uppy/webcam";
 import { Dashboard } from "@uppy/react";
 import RemoteSources from "@uppy/remote-sources";
-
+import Transloadit from "@uppy/transloadit";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "~/components/ui/dialog";
 import "@uppy/core/dist/style.min.css";
 import "@uppy/dashboard/dist/style.min.css";
 import "@uppy/webcam/dist/style.min.css";
-
-const requiredDocuments = [
-  "Medical License with NPI",
-  "State License",
-  "Liability Insurance",
-  "DEA License",
-  "Medical School Diploma",
-  "Immunization records",
-  "Resume",
-];
+import Link from "next/link";
+import { buttonVariants } from "~/components/ui/button";
 
 const DocUploadPage: React.FC = () => {
-  const [uploadedFiles, setUploadedFiles] = useState<string[]>([]);
+  const [demoStep, setDemoStep] = useState<number>(0);
+  const [open, setOpen] = useState(false);
+
+  const requiredDocuments = [
+    "Medical License with NPI",
+    demoStep == 1 ? "State License (Expired)" : "State License",
+    "Liability Insurance",
+    "DEA License",
+    "Medical School Diploma",
+    "Immunization records",
+    "Resume",
+  ];
   const uppy = new Uppy()
     .use(Webcam)
-    .use(RemoteSources, { companionUrl: "https://your-companion-url" });
+    .use(RemoteSources, {
+      companionUrl: "https://your-companion-url",
+      sources: ["Box", "Dropbox", "GoogleDrive", "OneDrive", "Url"],
+    })
+    .use(Transloadit, {
+      assemblyOptions: {
+        params: {
+          auth: { key: "3595d13af39b485b9ab798860b003df1" },
+          template_id: "9782abb40a274c3fa8862439dd2d101c",
+        },
+      },
+    });
 
-  const isFileUploaded = (documentName: string) => {
-    // Determine if the file corresponding to the documentName has been uploaded
-    // This is a simplified check based on file names
-    return uploadedFiles.some((fileName) => fileName.includes(documentName));
-  };
+  uppy.on("complete", (result) => {
+    console.log(result.successful);
+    if (result.successful) {
+      setOpen(true);
+      setDemoStep(demoStep + 1);
+    }
+  });
 
   return (
     <main>
-      <Card className="mx-auto mt-12 max-w-xl p-12 shadow">
+      <Card className="mx-auto max-w-xl px-12 shadow">
         <CardHeader>
-          <CardTitle className="text-xl">Locum Tenes Credentialing</CardTitle>
+          <CardTitle className="text-xl">
+            Complete Locum Tenes Credentialing
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <ul>
@@ -45,19 +76,59 @@ const DocUploadPage: React.FC = () => {
               <li key={index}>
                 <span
                   className={
-                    isFileUploaded(document) ? "text-green-500" : "text-red-500"
+                    demoStep > 1 ||
+                    (demoStep === 1 && document !== "State License (Expired)")
+                      ? "text-green-500"
+                      : "text-red-500"
                   }
                 >
-                  {isFileUploaded(document) ? "✔" : "✘"}
+                  {demoStep > 1 ||
+                  (demoStep === 1 && document !== "State License (Expired)")
+                    ? "✔"
+                    : "✘"}
                 </span>
-                {" " + document}
+                {"  " + document}
               </li>
             ))}
           </ul>
 
-          <Dashboard className="mt-8" uppy={uppy} plugins={["Webcam"]} />
+          {demoStep != 2 && (
+            <Dashboard
+              height={300}
+              className="mt-8"
+              uppy={uppy}
+              plugins={["Webcam"]}
+            />
+          )}
         </CardContent>
+        {demoStep == 2 && (
+          <CardFooter>
+            <Link
+              className={buttonVariants({ variant: "default" })}
+              href="/doc_form_filled"
+            >
+              {" "}
+              Continue{" "}
+            </Link>
+          </CardFooter>
+        )}
       </Card>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="mx-auto sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle className="text-center">
+              {" "}
+              {demoStep == 1 ? "Expired State License Detected" : "Success"}
+            </DialogTitle>
+          </DialogHeader>
+          <DialogDescription className="text-center">
+            {demoStep == 1
+              ? `The state license uploaded is expired. Please upload a valid state
+            license.`
+              : "Congratulations. All your documents have been processed successfully."}
+          </DialogDescription>
+        </DialogContent>
+      </Dialog>
     </main>
   );
 };
